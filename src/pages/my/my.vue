7fbce8ffd4412ac2e4fc20c33a11920643191239
yizhezhixing <2684965394@qq.com>
2023-10-22 21:29:51 +0800

    对“我的”进行布局

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,1 +1,48 @@
-<template></template>
+<template>
+  <div class="contanier">
+    <div class="avatar">
+      <u-avatar
+        src="https://th.bing.com/th/id/OIP.rEU63q6oHc08uv3uh2_algHaEK?w=293&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
+        size = "150rpx"
+      ></u-avatar>
+    </div>
+    <div class="head">
+      <u-row gutter="5">
+        <u-col span="1"> </u-col>
+        <u-col span="4">User_name</u-col>
+        <u-col span="4">User_information</u-col>
+      </u-row>
+    </div>
+    <div class="cell">
+      <u-cell-group title="个人工具">
+        <u-cell
+          icon="setting-fill"
+          title="工具1"
+          label="解释"
+          :arrow="true"
+          value="click here"
+          @click=""
+        ></u-cell>
+        <u-cell
+          icon="photo"
+          title="工具2"
+          label="解释"
+          :arrow="true"
+          hover-class="cell-hover-class"
+        ></u-cell>
+      </u-cell-group>
+    </div>
+    <div class="test">
+      <u-tag text="hello" shape="circle" closable:show="show" @close=""></u-tag>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.avatar{
+  margin-top: 10%;
+  margin-left: 15%;
+}
+</style>
